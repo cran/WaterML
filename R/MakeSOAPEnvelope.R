6585f9a1b2a4f5866c052ca89b91619b73f1af9e
Jiri Kadlec <jirikadlec2@gmail.com>
2021-04-10 07:10:10 +0000

    version 1.9.8

--- R/MakeSOAPEnvelope.R
+++ R/MakeSOAPEnvelope.R
@@ -26,12 +26,19 @@
 #' myEnvelope <- MakeSOAPEnvelope("http://www.cuahsi.org/his/1.1/ws/", "GetSitesObject")
 #' SOAPAction <- "http://www.cuahsi.org/his/1.1/ws/GetSitesObject"
 #' url <- "http://hydrodata.info/chmi-d/cuahsi_1_1.asmx"
-#' response <- POST(url, body = myEnvelope,
-#'                  add_headers("Content-Type" = "text/xml", "SOAPAction" = SOAPAction),
-#'                  verbose())
-#' status.code <- http_status(response)$category
-#' WaterML <- xmlParse(response)
-#' WaterML
+#' downloaded <- FALSE
+#' err <- tryCatch({
+#'   response <- POST(url, body = myEnvelope,
+#'                    add_headers("Content-Type" = "text/xml", "SOAPAction" = SOAPAction))
+#'   downloaded <- TRUE
+#' },error = function(e) {
+#'   print(conditionMessage(e))
+#' })
+#' if (downloaded) {
+#'   status.code <- http_status(response)$category
+#'   WaterML <- xmlParse(response)
+#'   WaterML
+#' }
 
 MakeSOAPEnvelope <- function(CUAHSINamespace, MethodName, parameters=NULL) {
 
